c4cf6a431fd97fc9d86224835b23c342d963260c
Nicholas <najernigin@gmail.com>
2023-11-28 14:54:22 -0600

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,19 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
-}
+  return (<nav>NavBar
+    <Links />
+  </nav>);
 
+}
+function Links(props) {
+  return (
+    <div>
+      <a href='#home'>home</a>
+      <a href='#about'>about</a>
+    </div>
+
+  );
+
+}
 export default NavBar;
